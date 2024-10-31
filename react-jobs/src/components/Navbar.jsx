a03de9cd5f225a4885c0f1de9937b87419c8f64e
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-10-31 18:30:55 +0000

    Refactor Navbar link class in Navbar.jsx

--- react-jobs/src/components/Navbar.jsx
+++ react-jobs/src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import logo from '../assets/images/logo.png'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const linkClass = (({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2')
+
     return (
         <nav className="bg-indigo-700 border-b border-indigo-500">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -27,17 +30,17 @@ const Navbar = () => {
                             <div className="flex space-x-2">
                                 <NavLink
                                     to="/"
-                                    className={({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'}
+                                    className={linkClass}
                                 >Home
                                 </NavLink>
                                 <NavLink
                                     to="/jobs"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={linkClass}
                                 >Jobs
                                 </NavLink>
                                 <NavLink
                                     to="/add-job"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={linkClass}
                                 >Add Job
                                 </NavLink>
                             </div>

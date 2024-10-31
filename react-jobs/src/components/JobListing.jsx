6f7041bbae0fd4158e6c7543cd20c9937d2914ba
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-10-31 00:07:22 +0000

    Add show/hide functionality for job description

--- react-jobs/src/components/JobListing.jsx
+++ react-jobs/src/components/JobListing.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
 
 const JobListing = ({ job }) => {
+    const [showFullDescription, setShowFullDescription] = useState(false);
+
+    let description = job.description;
+
+    if (!showFullDescription) {
+        description = job.description.substring(0, 90) + '...';
+    }
+
     return (
         <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -10,25 +19,28 @@ const JobListing = ({ job }) => {
                 </div>
 
                 <div className="mb-5">
-                    {job.description}
+                    {description}
                 </div>
+
+                <button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{showFullDescription ? 'Less' : 'More'}</button>
 
                 <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
-                <div className="border border-gray-100 mb-5"></div>
+            </div> {/* Add closing tag for the div element */}
 
-                <div className="flex flex-col lg:flex-row justify-between mb-4">
-                    <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg"></i>
-                        {job.location}
-                    </div>
-                    <a
-                        href={`/job/${job.id}`}
-                        className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                    >
-                        Read More
-                    </a>
+            <div className="border border-gray-100 mb-5"></div>
+
+            <div className="flex flex-col lg:flex-row justify-between mb-4">
+                <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    {job.location}
                 </div>
+                <a
+                    href={`/job/${job.id}`}
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                    Read More
+                </a>
             </div>
         </div>
     )

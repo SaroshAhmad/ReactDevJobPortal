4d89231f2bcfd81b91d790510d1cbab2fd84f123
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-02 01:07:29 +0000

    Update EditJobPage and JobPage components

--- react-jobs/src/pages/EditJobPage.jsx
+++ react-jobs/src/pages/EditJobPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom'
 
 const EditJobPage = () => {
+    const job = useLoaderData();
     return (
-        <div>EditJobPage</div>
+        <div>{job.title}</div>
     )
 }
 
